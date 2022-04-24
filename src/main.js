932494c087c4cea4fd44fb295f8083dd6c9bce29
‘liucong‘ <719071413@qq.com>
2022-04-24 18:05:08 +0800

    111

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import element from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css'
+createApp(App).use(element).use(store).use(router).mount('#app')
